7003b8e4ec6bee15d81f3c8c102388408cb01c89
Mark Nelson <marknels@hawaii.edu>
2023-01-15 11:38:50 -1000

    Initial template

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ee205_lab2d_octocat
+ee205_lab2d_octocat
+===================
+
+Your source is in `octocat.c`
+
+To compile it: `$ gcc -o octocat octocat.c`
+
+To run it:  
---
+++ octocat.c
@@ -0,0 +1,19 @@
+///////////////////////////////////////////////////////////////////////////////
+//          University of Hawaii, College of Engineering
+//          Lab 2d - Octocat - EE 205 - Spring 2023
+//
+/// Nothing is beyond my reach.
+///
+/// The intent of this program is to:
+///    - Write a simple algorithm in C
+///    - Remember everything you've forgotten about programming
+///
+/// Compile: $ gcc -o octocat octocat.c
+///
+/// Example:
+///   $ ./octocat
+///   XXXXX
+///
+/// @file    octocat.c
+/// @author  @todo yourName <@todo yourMail@hawaii.edu>
+///////////////////////////////////////////////////////////////////////////////
